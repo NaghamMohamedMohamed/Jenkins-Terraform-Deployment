98109c15a9afefcde282150ba07be9dcd0d575a7
Nagham Mohamed <107771328+NaghamMohamedMohamed@users.noreply.github.com>
2025-05-06 21:48:04 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "jenkins-nagham"
+    bucket         = "tf-lab3-bucket-nagham"
     key            = "vpc-setup/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile   = true 
